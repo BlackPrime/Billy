6a7cd28ce8ff604b996331bfe8acfc7a1ea0ea2b
Blackwolf <amin.sdouga@epita.fr>
2015-06-25 15:52:07 +0200

    debut de notre projet Billy

---
+++ Scripts/Billy/BillyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillyController : MonoBehaviour
+{
+
+	private float h;
+	private float v;
+	private Rigidbody2D playerRigidbody;
+
+	private void Start ()
+	{
+		playerRigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		
+	}
+}
