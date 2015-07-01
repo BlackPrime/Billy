99f58b10629ba16e11feda9e43d0b62aa3b4338e
Blackwolf <amin.sdouga@epita.fr>
2015-07-01 21:04:25 +0200

    main menu progressing

---
+++ Scripts/MainMenu/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public Texture2D cursorTexture;
+	public Vector2 offset = Vector2.zero;
+
+	void Start()
+	{
+		Cursor.SetCursor(cursorTexture, offset, CursorMode.ForceSoftware);
+	}
+}
