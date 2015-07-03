2f8b63b58b9f8b6b0bb64f83aae56acb6f693864
Blackwolf <amin.sdouga@epita.fr>
2015-07-03 20:08:31 +0200

    avancement du mainMenu

---
+++ Scripts/ButtonLoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonLoadScene : MonoBehaviour, IPointerDownHandler
+{
+	public string scene = "MainMenu";
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Application.LoadLevel(scene);
+	}
+}
---
+++ Scripts/MainMenu/ButtonQuit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonQuit : MonoBehaviour, IPointerDownHandler
+{
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Application.Quit();
+	}
+}
